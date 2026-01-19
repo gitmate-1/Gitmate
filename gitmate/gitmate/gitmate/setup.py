185b4aab43c8842adcaf7c21210d61b41aa57d07
gitmate-1 <aaaaaaadi29@gmail.com>
2026-01-19 12:31:34 +0530

    Update setup.py

--- gitmate/gitmate/gitmate/setup.py
+++ gitmate/gitmate/gitmate/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="gitmate",
+    name="gitmate-cli",
     version="1.0.0",
     author="Your Name",
     description="🤖 Smart Git Automation CLI Tool — Auto commit, push, and manage repos with style!",
